f728217862acfc359752a16a62c14b6c9027a154
Michael Sault <msaul025@uottawa.ca>
2020-05-22 01:30:06 -0400

    sign up functionality working
    -next work on log in functionality

--- php login/index.php
+++ php login/index.php
@@ -1,4 +1,21 @@
 <!DOCTYPE HTML>
+
+<?php
+    if (isset($_POST['submitted'])){
+        $db_connection = pg_connect("host=localhost dbname=postgres user=postgres password=selenagomez");
+
+        $user = $_POST['user'];
+        $pass = $_POST['pass'];
+
+        $sqlinsert = "INSERT INTO usertable (username, password) VALUES ('$user', '$pass')";
+
+        if (!pg_query($db_connection, $sqlinsert)) {
+            die('error creating new user');
+        }
+        $newrecord = "1 record added to database";
+    }
+?>
+
 <html>
 <head>
     <title>PHP Login</title>
@@ -6,9 +23,22 @@
 
 <body>
 
+
+<h1> Create A User: </h1>
+
+<form method = "post" action = "index.php">
+<input type = "hidden" name = "submitted" value = "true"/>
+<fieldset>
+    <legend><b>Sign Up:</b></legend>
+    <label>Username: <input type = "text" name = "user"/> </label>
+    <label>Password: <input type = "text" name = "pass"/> </label>
+</fieldset>
+<br>
+<input type = "submit" value = "Submit"/>
+</form>
+
 <?php
-    $db_connection = pg_connect("host=localhost dbname=postgres user=postgres password=selenagomez");
-    $result = pg_query($db_connection, "SELECT * FROM Users");
+    echo $newrecord;
 ?>
 
 </body>
