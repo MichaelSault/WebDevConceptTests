c301c1b5bd5d427196897cd2497087ff6fda1fb2
Michael Sault <msaul025@uottawa.ca>
2020-05-27 23:58:56 -0400

    started login function

--- php login v2/includes/login.php
+++ php login v2/includes/login.php
@@ -1,14 +1,39 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-    <title>PHP Login</title>
-</head>
-
-<body>
-
 <?php
-    echo 'Hello World!';
-?>
 
-</body>
-</html>
+    if (isset($_POST['login-submit'])) {
+        require 'dbh.php';
+        $userORemail = $_POST['uid'];
+        $password = $_POST['pwd'];
+
+        if (empty($userORemail)|| empty($password)) {
+            header("Location: ../signup.php?error=emptyfields");
+            exit(); //stops the script if an error 
+        }
+        else {
+            $sql = "SELECT * FROM users WHERE unameUsers=? OR emailUsers=?";
+            $stmt = mysqli_stmt_init($conn);
+
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                header("Location: ../signup.php?error=sqlerror");
+                exit();
+            }
+            else {
+                exit(); //will do something later, ran out of time for tonight...
+            }
+        }
+
+    }
+    else {
+        header("Location:../index.php?error=notsignedin");
+        exit();
+    }
+
+
+
+
+
+
+
+
+
+    //INSERT INTO users (unameUsers, emailUsers, passUsers)
